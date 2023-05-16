30048f8dbd812409e542361a70f26f202286efcf
Devang Mehta <>
2023-05-16 16:47:49 +0530

    updated code for grid traveller problem

--- dynamic_programming/grid_traversal.py
+++ dynamic_programming/grid_traversal.py
@@ -5,22 +5,16 @@ def traverse(i, j, m, n, result, cache):
         return 0
     if j >= n:
         return 0
+
     if (i, j) in cache.keys():
         return cache[(i,j)]
     if i == m-1 and j == n-1:
         return 1
 
-    # second operand is for moving down one step
-    # third operand is for moving right one step
-    # result = traverse(i+1, j, m, n, result) + traverse(i, j+1, m, n, result)
-    down_traversal = traverse(i+1, j, m, n, result, cache)
-    if (i+1, j) not in cache.keys():
-        cache[(i+1, j)] = down_traversal
-    right_traversal = traverse(i, j+1, m, n, result, cache)
-    if (i, j+1) not in cache.keys():
-        cache[(i, j+1)] = right_traversal
-
-    return result + down_traversal + right_traversal
+    # first operand is for moving down one step
+    # second operand is for moving right one step
+    cache[(i, j)] = traverse(i+1, j, m, n, result, cache) + traverse(i, j+1, m, n, result, cache)
+    return cache[(i, j)]
 
 def start_traversal(m, n):
     # we will be starting at the top left corner of the grid
@@ -31,6 +25,6 @@ def start_traversal(m, n):
     cache = {}
     return traverse(i, j, m, n, result, cache)
 
-m = 20
-n = 20
+m = 18
+n = 18
 print(m, n, start_traversal(m, n))
